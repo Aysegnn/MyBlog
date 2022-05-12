05663df1e7d2086b9ba5917c34f67ce4fdb1dcc8
Aysegnn <ayse.gnn3861@gmail.com>
2022-05-12 22:00:37 +0300

    Jetstream ile auth işlemi gerçekleştirildi.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 //Admin Routes
-Route::get('/', function () { return view('admin.homepage');});
+Route::get('/', function () { return view('welcome');
+})->name('welcome');
+Route::get('/dashboard', function () { return view('admin.dashboard');
+});
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.homepage');
+    })->name('admin.dashboard');
+});
